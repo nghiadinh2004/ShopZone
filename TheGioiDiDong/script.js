ffc1f9a4e2acc394b574fb72f9c3c17119c6167f
nghiadinh2004 <trongnghiahm123@gmail.com>
2023-10-09 22:50:10 +0700

    TrongNghia

--- TheGioiDiDong/script.js
+++ TheGioiDiDong/script.js
@@ -1,4 +1,4 @@
-const addressbtn = document.querySelector('#address-form')
+﻿const addressbtn = document.querySelector('#address-form')
 const addresclose = document.querySelector('#address-close')
 
 
@@ -9,8 +9,35 @@ addresclose.addEventListener("click", function () {
     document.querySelector('.address-form').style.display = "none"
 })
 //slider-----------------------------------------------------
+/*P6 - P7 - P8*/
 const rightbtn = document.querySelector('.fa-chevron-right')
-rightbtn.addEventListener ("Click", function(){
-    console.log("ok")
+rightbtn.addEventListener("Click", function () {
+    
     document.querySelector(".slider-content-left-top").style.right = "100%"
 })
+
+
+/*P9*/
+//-----------------------------------------------------slider-product------------------------
+
+const rightbtntwo = document.querySelector('.fa-chevron-right-two')
+const leftbtntwo = document.querySelector('.fa-chevron-left-two')
+const imgNumbertwo = document.querySelectorAll('.slider-product-one-content-items')
+//console.log(rightbtntwo) 
+//console.log(leftbtntwo)
+rightbtntwo.addEventListener ("click", function(){
+    index = index+1
+    if(index>imgNumbertwo.length-1){
+        index=0
+    }
+    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
+})
+leftbtntwo.addEventListener ("click", function(){
+    index = index-1
+    if(index<=0){
+        index=imgNumbertwo.length-1
+    }
+    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
+})
+
+
