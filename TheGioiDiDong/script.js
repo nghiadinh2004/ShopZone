bfdec8ce2a574637142e2655aa74ffed4962a6d0
nghiadinh2004 <trongnghiahm123@gmail.com>
2023-10-10 11:38:43 +0700

    TrongNghia

--- TheGioiDiDong/script.js
+++ TheGioiDiDong/script.js
@@ -20,24 +20,22 @@ rightbtn.addEventListener("Click", function () {
 /*P9*/
 //-----------------------------------------------------slider-product------------------------
 
-const rightbtntwo = document.querySelector('.fa-chevron-right-two')
-const leftbtntwo = document.querySelector('.fa-chevron-left-two')
-const imgNumbertwo = document.querySelectorAll('.slider-product-one-content-items')
-//console.log(rightbtntwo) 
-//console.log(leftbtntwo)
-rightbtntwo.addEventListener ("click", function(){
-    index = index+1
-    if(index>imgNumbertwo.length-1){
-        index=0
-    }
-    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
-})
-leftbtntwo.addEventListener ("click", function(){
-    index = index-1
-    if(index<=0){
-        index=imgNumbertwo.length-1
-    }
-    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
-})
+const rightbtntwo = document.querySelector('.fa-chevron-right-two');
+const leftbtntwo = document.querySelector('.fa-chevron-left-two');
+const itemsContent = document.querySelector(".slider-product-one-content-items-content");
+const imgNumbertwo = document.querySelectorAll('.slider-product-one-content-items');
+let index = 0; // Initialize the index
+
+rightbtntwo.addEventListener("click", function () {
+    index = (index + 1) % imgNumbertwo.length;
+    itemsContent.style.right = index * 100 + "%";
+});
+
+leftbtntwo.addEventListener("click", function () {
+    index = (index - 1 + imgNumbertwo.length) % imgNumbertwo.length;
+    itemsContent.style.right = index * 100 + "%";
+});
+
+
 
 
